48edd2d37f48ed33fecb91ea9192cef606b4af78
9swampy <github@prims.com>
2022-02-22 23:16:21 +0000

    Debug params used from inside docker.

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,13 +19,17 @@ fi
 #LOCAL_PATH
 
 ARGS="${INPUT_MIRROR_ARGS}"
+echo "Uri=${URI}"
+echo "UserName=${INPUT_USERNAME}"
+echo "Local=${INPUT_LOCAL_PATH}"
+echo "Remote=${INPUT_REMOTE_PATH}"
 
+#--reverse sends file to the server from the LOCAL_PATH
 lftp $URI <<- TRANSFER
     set ssl:priority ${INPUT_SSL_PRIORITY}
     set ssl:verify-certificate ${INPUT_SSL_VERIFY_CERT}
     set ftp:ssl-force ${INPUT_SSL_FORCE}
     user $INPUT_USERNAME $INPUT_PASSWORD
 
-    #--reverse sends file to the server from the LOCAL_PATH
     mirror --verbose --reverse $ARGS $INPUT_LOCAL_PATH $INPUT_REMOTE_PATH
 TRANSFER
