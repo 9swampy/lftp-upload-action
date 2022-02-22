81a3cee6e4a7fbe14ab21a1fda205bde78f9b2b3
9swampy <github@prims.com>
2022-02-22 23:40:54 +0000

    Hardcode and debug everything (except pwd)

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,15 +18,25 @@ fi
 #REMOTE_PATH
 #LOCAL_PATH
 
+URI="sftp://home416919653.1and1-data.host:22"
+INPUT_USERNAME="u69050957-BlazorWeb"
+INPUT_LOCAL_PATH="/home/runner/work/AzureWebsiteCore/AzureWebsiteCore/BlazorWeb/Api/"
+INPUT_REMOTE_PATH="/"
+INPUT_SSL_VERIFY_CERT=$false
+INPUT_SSL_FORCE=$true
+
 ARGS="${INPUT_MIRROR_ARGS}"
 echo "Uri=${URI}"
 echo "UserName=${INPUT_USERNAME}"
 echo "Local=${INPUT_LOCAL_PATH}"
 echo "Remote=${INPUT_REMOTE_PATH}"
+echo "INPUT_SSL_VERIFY_CERT=${INPUT_SSL_VERIFY_CERT}"
+echo "INPUT_SSL_FORCE=${INPUT_SSL_FORCE}"
+echo "ARGS=${ARGS}"
 
 #--reverse sends file to the server from the LOCAL_PATH
-lftp $URI <<- TRANSFER
-    set ssl:priority ${INPUT_SSL_PRIORITY}
+#set ssl:priority ${INPUT_SSL_PRIORITY}
+lftp $URI << TRANSFER    
     set ssl:verify-certificate ${INPUT_SSL_VERIFY_CERT}
     set ftp:ssl-force ${INPUT_SSL_FORCE}
     user $INPUT_USERNAME $INPUT_PASSWORD
